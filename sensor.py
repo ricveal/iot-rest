8e4fa846ff2fcf4ef8c29d03e4ed1450e90a1e0f
ricveal <ricvea@gmail.com>
2016-05-18 19:45:47 +0200

    Configuration

--- sensor.py
+++ sensor.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 from app import db, ma
 
-
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temperature = db.Column(db.Float())
     date = db.Column(db.DateTime)
-
 
     def __init__(self, temperature, date=None):
         self.temperature = temperature
